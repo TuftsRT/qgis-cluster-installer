a4ec08900fd083eae2a9ce532d52df338483e9a9
Uku-Kaspar Uustalu <uku-kaspar.uustalu@tufts.edu>
2023-11-27 15:37:11 -0500

    move qgis settings to etc

--- launch.sh
+++ launch.sh
@@ -10,7 +10,7 @@ conda activate "%PREFIX%/envs/$ENVIRONMENT"
 # set gdal and qgis environment variables
 export GDAL_NUM_THREADS="ALL_CPUS"
 export GDAL_CACHEMAX="25%"
-export QGIS_GLOBAL_SETTINGS_FILE="%PREFIX%/config/qgis_global_settings.ini"
+export QGIS_GLOBAL_SETTINGS_FILE="%PREFIX%/etc/qgis_global_settings.ini"
 
 # launch qgis
 qgis --nologo --noversioncheck
