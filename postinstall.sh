c90eae142b323bbe00e85bcf7136172f25ce1310
Uku-Kaspar Uustalu <uku-kaspar.uustalu@tufts.edu>
2023-11-28 16:48:01 -0500

    update postinstall script

--- postinstall.sh
+++ postinstall.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sed -i "s|%PREFIX%|$PREFIX|g" "$PREFIX/utils/initialize.sh"
+sed -i "s|%PREFIX%|$PREFIX|g" "$PREFIX/shell/initialize.sh"
 sed -i "s|%PREFIX%|$PREFIX|g" "$PREFIX/ood/launch.sh"
